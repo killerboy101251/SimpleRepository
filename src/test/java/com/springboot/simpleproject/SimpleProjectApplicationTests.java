6eea30cc0ccb7066e17772db1c085f200421659b
mohanreddym <molakathallamohan@gmail.com>
2023-01-19 15:02:24 +0530

    simple spring boot project

---
+++ src/test/java/com/springboot/simpleproject/SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.simpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
